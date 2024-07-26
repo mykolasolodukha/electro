14c008bdb46fdab6e2fadda6d6de44622cd7df0b
Mykola Solodukha <mykola@mykolasolodukha.me>
2024-07-26 16:23:39 +0300

    :ambulance: Disable the `!version` command
    Because `electro`, if installed, doesn't have the `pyproject.toml` file.

--- electro/__version__.py
+++ electro/__version__.py
@@ -5,12 +5,12 @@ Separated from the `__init__` one because some tools use `__init__` internally
 (e.g. Pycharm Interactive console: `pycharm_matplotlib_backend`) and that creates problems.
 """
 
-import tomllib
-from pathlib import Path
-
-# Get the version
-with open(Path(__file__).parent.parent / "pyproject.toml", "rb") as file:
-    __version__ = tomllib.load(file)["tool"]["poetry"]["version"]
-
-
-__all__ = ["__version__"]
+# import tomllib
+# from pathlib import Path
+#
+# # Get the version
+# with open(Path(__file__).parent / "pyproject.toml", "rb") as file:
+#     __version__ = tomllib.load(file)["tool"]["poetry"]["version"]
+#
+#
+# __all__ = ["__version__"]
--- electro/bot.py
+++ electro/bot.py
@@ -3,12 +3,12 @@
 import discord
 from discord.ext import commands
 
-from .__version__ import __version__
+# from .__version__ import __version__
 from .settings import settings
 
 
 # Use this hacks to prevent the commands from being added multiple times
-__VERSION_COMMAND_ENABLED = False
+# __VERSION_COMMAND_ENABLED = False
 __PING_COMMAND_ENABLED = False
 
 intents = discord.Intents.default()
@@ -19,13 +19,13 @@ intents.message_content = True
 bot = commands.Bot(command_prefix=settings.BOT_COMMAND_PREFIX, intents=intents)
 
 # Enable the `!version` and `!ping` commands on the lower level than the `electro` Framework does
-if not __VERSION_COMMAND_ENABLED:
-    @bot.command(name="version")
-    async def get_version(ctx):
-        await ctx.send(f"Version: {__version__}")
-
-
-    __VERSION_COMMAND_ENABLED = True
+# if not __VERSION_COMMAND_ENABLED:
+#     @bot.command(name="version")
+#     async def get_version(ctx):
+#         await ctx.send(f"Version: {__version__}")
+#
+#
+#     __VERSION_COMMAND_ENABLED = True
 
 if not __PING_COMMAND_ENABLED:
     @bot.command(name="ping")
